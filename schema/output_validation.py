bbe525ea5698d3c29d7e4ad61f220aed76e9395e
Jugal Lachhwani <144345250+Jugal-lachhwani@users.noreply.github.com>
2025-08-02 00:45:38 +0530

    Delete schema directory

--- schema/output_validation.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Dict
-
-class PredictionResponse(BaseModel):
-    predicted_category: float = Field(
-        ...,
-        description="The predicted insurance premium category",
-        example="High"
-    )
